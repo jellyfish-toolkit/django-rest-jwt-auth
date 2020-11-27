29f11fba3c994b586958a0872737adf41fc650bf
Yevhen Kozlov <yevhen.kozlov.ua@gmail.com>
2020-11-27 18:04:59 +0200

    PostgreSQL+pREST docker-compose

--- restauth/restauth/settings.py
+++ restauth/restauth/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'restauth.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'test',
+        'USER' : 'test',
+        'PASSWORD' : 'password',
+        'HOST' : '127.0.0.1',
+        'PORT' : '5432',
     }
 }
 
@@ -123,4 +127,4 @@ STATIC_URL = '/static/'
 # Settings for 'django_rest_auth'
 JWT_SECRET = 'super-secret-key'
 JWT_ALGORITHM = 'HS256'
-JWT_ROLE = 'postgres'
+JWT_ROLE = DATABASES['default']['USER']
